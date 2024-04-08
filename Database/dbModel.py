810cf6218ef5075e8724d6d5884c8799b7b0bae3
Jiseungmin <wltmdals0118@sunmoon.ac.kr>
2024-04-08 16:18:27 +0900

    merge

--- Database/dbModel.py
+++ Database/dbModel.py
@@ -1,1 +1,10 @@
-# TODO 
+from pymongo import MongoClient
+
+client = MongoClient("mongodb+srv://theBuleocean:opensource418@newssun.ts97rhi.mongodb.net/")
+db = client['news_test']
+url_data = db.URLDATA.find()
+
+
+for data in url_data:
+  print(data)
+    
